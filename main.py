b131ed740cee77ff25091f28bacabe076ce273f1
baconlolz <evanjoelsanchez@gmail.com>
2021-09-09 21:42:53 -0700

    Added Evan.html and Abby.html

--- main.py
+++ main.py
@@ -10,34 +10,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/Abby/')
-def Abby():
-    return render_template("Abby.html")
-
-@app.route('/Evan/')
+@app.route('/Evan', methods=['GET', 'POST'])
 def Evan():
-    return render_template("Evan.html")
-
-
-@app.route('/Natalie/')
-def Natalie():
-    return render_template("Natalie.html")
-
-@app.route('/Hassan/')
-def Hassan():
-    return render_template("Hassan.html")
-
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
     # submit button has been pushed
     if request.form:
         name = request.form.get("name")
         if len(name) != 0:  # input field has content
-            return render_template("greet.html", name=name)
+            return render_template("Evan.html", name=name)
     # starting and empty input default
-    return render_template("greet.html", name="World")
+    return render_template("Evan.html", name="World")
 
-
+@app.route('/Abby', methods=['GET', 'POST'])
+def Abby():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("Abby.html", name=name)
+    # starting and empty input default
+    return render_template("Abby.html", name="World")
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
