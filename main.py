4107583ee06bc3807b1d1dfa808f7a9bafe2edfa
nataliebeckwith <natalieb60251@stu.powayusd.com>
2021-09-14 13:29:19 -0700

    hi

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ def greet():
     # starting and empty input default
     return render_template("greet.html", name="World")
 
-
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
